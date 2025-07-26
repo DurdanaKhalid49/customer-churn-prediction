bc5467d3da5e6d5ea38b58fc3ff3c481ead628c8
Durdana_Khalid <durdanakhalid98@gmail.com>
2025-07-26 12:42:56 +0500

    Update app.py

--- Flask_Web_App/app.py
+++ Flask_Web_App/app.py
@@ -44,5 +44,5 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # use Railway port or default to 5000
+    port = int(os.environ.get("PORT", 8080))  # use Railway port or default to 8080
     app.run(host="0.0.0.0", port=port)  
